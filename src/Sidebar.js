fbe40fa9bc2ccf89061ae4b0d071ff3a1d9387f8
Sahil Samantaray <sahil13399@gmail.com>
2021-03-17 23:35:57 +0530

    Sidebar options complete

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,8 +2,18 @@ import { Button } from '@material-ui/core'
 import React from 'react'
 import './Sidebar.css'
 import AddIcon from '@material-ui/icons/Add';
-import SidebarOption from './SidebarOption';
+import SidebarOption from './SidebarOption.js';
 import InboxIcon from '@material-ui/icons/Inbox';
+import StarIcon from '@material-ui/icons/Star';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import LabelImportantIcon from '@material-ui/icons/LabelImportant';
+import NearMeIcon from '@material-ui/icons/NearMe';
+import NoteIcon from '@material-ui/icons/Note';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import {IconButton } from '@material-ui/core';
+import PersonIcon from '@material-ui/icons/Person';
+import DuoIcon from '@material-ui/icons/Duo';
+import PhoneIcon from '@material-ui/icons/Phone';
 
 function Sidebar() {
     return (
@@ -11,8 +21,27 @@ function Sidebar() {
             <Button startIcon={<AddIcon fontSize="large" />} className="sidebar_compose" >
                 Compose
             </Button>
-            <SidebarOption icon={InboxIcon} title = "Inbox" number = {34}/>
+            <SidebarOption Icon={InboxIcon} title = "Inbox" num = {34} selected = {false}/>
+            <SidebarOption Icon={StarIcon} title = "Starred" num = {34} selected = {false}/>
+            <SidebarOption Icon={AccessTimeIcon} title = "Snoozed" num = {34} selected = {false}/>
+            <SidebarOption Icon={LabelImportantIcon} title = "Important" num = {34} selected = {false}/>
+            <SidebarOption Icon={NearMeIcon} title = "Sent" num = {34} selected = {false}/>
+            <SidebarOption Icon={NoteIcon} title = "Drafts" num = {34} selected = {false}/>
+            <SidebarOption Icon={ExpandMoreIcon} title = "More" num = {34} selected = {false}/>
+        <div className="sidebar_footer"> 
+            <div className = "sidebar_footerIcons">
+                <IconButton>
+                    <PersonIcon />
+                </IconButton>
+                <IconButton>
+                    <DuoIcon />
+                </IconButton>
+                <IconButton>
+                    <PhoneIcon />
+                </IconButton>
+            </div>
         </div>
+   </div>
     )
 }
 
--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './SidebarOption.css'
 
-function SidebarOption() {
+function SidebarOption({Icon,title,num,selected}) {
     return (
-        <div className = "sidebarOption">
-            
+        <div className = {`sidebarOption ${selected && "sidebarOption--active"}`}>
+            <Icon />
+            <h3>{title}</h3>
+            <p>{num}</p>
         </div>
     )
 }
 
-export default SidebarOption
+export default SidebarOption;
