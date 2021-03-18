105c586931b68c022ec41ca64bd376d7bfb2535a
Sahil Samantaray <sahil13399@gmail.com>
2021-03-18 15:41:51 +0530

    Email List Sections implemented

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import EmailList from './EmailList';
+import Mail from './Mail';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Header />
-      <Sidebar />
+      <div className="appBody">
+        <Sidebar />
+        <Switch>
+          <Route path = "/">
+            <EmailList />
+          </Route>
+          <Route path="/mail">
+            <Mail />
+          </Route>
+        </Switch>
+      </div>
     </div>
+    </Router>
   );
 }
 
---
+++ src/EmailList.js
@@ -0,0 +1,57 @@
+import { Checkbox,IconButton } from '@material-ui/core'
+import React from 'react'
+import './EmailList.css'
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import RedoIcon from '@material-ui/icons/Redo';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SettingsIcon from '@material-ui/icons/Settings';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import KeyboardHideIcon from '@material-ui/icons/KeyboardHide';
+import Section from './Section.js'
+import InboxIcon from '@material-ui/icons/Inbox';
+import PeopleIcon from '@material-ui/icons/People';
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+
+function EmailList() {
+    return (
+        <div className = "emaillist">
+            <div className = "emaillist_settings">
+                <div className = "emaillist_settings_left">
+                    <Checkbox />
+                    <IconButton>
+                        <ArrowDropDownIcon />
+                    </IconButton>
+                    <IconButton>
+                        <RedoIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+                <div className = "emaillist_settings_right">
+                    <IconButton>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChevronRightIcon />
+                    </IconButton>
+                    <IconButton>
+                        <KeyboardHideIcon />
+                    </IconButton>
+                    <IconButton>
+                        <SettingsIcon />
+                    </IconButton>
+                </div>
+
+            </div>
+            <div className = "emaillist_sections">
+                    <Section Icon = {InboxIcon} title = "Primary" color = "red" selected = {true} />
+                    <Section Icon = {PeopleIcon} title = "Social" color = "#1A73E8" selected = {false} />
+                    <Section Icon = {LocalOfferIcon} title = "Promotions" color = "green" selected = {false} />
+                </div>           
+        </div>
+    )
+}
+
+export default EmailList
